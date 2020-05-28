0f11e4eceac3d940c440e2892916ea32ba9a7400
Ylli Prifti <ylli.prifti@anaplan.com>
2020-05-29 00:06:06 +0100

    updating README

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name="dr-web-engine",  # Replace with your own username
-        version="0.0.1",
+        version="V0.1.beta",
         author="Ylli Prifti",
         author_email="ylli@prifti.us",
         description="Data Retreival Web Engine - Queryable Web Scrap engine build on python based on lxml package and using JSON as query construct.",
         long_description=long_description,
         long_description_content_type="text/markdown",
         url="https://github.com/ylliprifti/dr-web-engine",
+        download_url='https://github.com/ylliprifti/dr-web-engine/archive/V0.1.beta.tar.gz',
         packages=setuptools.find_packages(),
         classifiers=[
             "Programming Language :: Python :: 3",
